2e956d09ab85c90709efe19db2e64abba87e76ee
badger169 <67238123+badger169@users.noreply.github.com>
2020-06-21 14:05:37 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_ThomasJonathan.py
@@ -0,0 +1,15 @@
+#Basic Math Calculation Code
+#06/19/2020
+#CTI-110 P2T1 - Sales Prediction
+#Jonathan Thomas
+
+
+#Get the projected total sales.
+value1 = float(input('Enter your first number: '))
+value2 = float(input('Enter your second number: '))
+
+#calculate the two values
+sum = (value1 + value2)
+
+#Dislpay the sum.
+print('The sum is', sum)
